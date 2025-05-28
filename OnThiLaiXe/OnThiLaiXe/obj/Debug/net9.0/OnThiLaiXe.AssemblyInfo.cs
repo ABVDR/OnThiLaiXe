7875a74b3be5af26b468cfd803905386c98e33f0
PHAN DINH TUAN <pdtuan04@gmail.com>
2025-05-28 14:53:01 +0700

    sualoitktolst

--- OnThiLaiXe/OnThiLaiXe/obj/Debug/net9.0/OnThiLaiXe.AssemblyInfo.cs
+++ OnThiLaiXe/OnThiLaiXe/obj/Debug/net9.0/OnThiLaiXe.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnThiLaiXe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46aeaf8afa4253e24f0d010ac458bf0adab51820")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13876ca13c794ff241fc3a90084caea09105ad6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnThiLaiXe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnThiLaiXe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
