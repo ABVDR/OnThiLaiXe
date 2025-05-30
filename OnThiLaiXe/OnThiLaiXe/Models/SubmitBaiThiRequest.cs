ef800063c00c7ed8ed2ac7eac269dd390b1995bc
ABVDR <ngocchau0845@gmail.com>
2025-05-30 16:37:04 +0700

    them luyen lai cau sai

---
+++ OnThiLaiXe/OnThiLaiXe/Models/SubmitBaiThiRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace OnThiLaiXe.Models
+{
+    public class SubmitBaiThiRequest
+    {
+        public int BaiThiId { get; set; }
+        public Dictionary<int, string> Answers { get; set; }
+    }
+}
